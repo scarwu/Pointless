2e1e4f59e6f222d41e20fca1762b121bac5c43d1
ScarWu <xneriscool@gmail.com>
2012-04-24 00:51:04 +0800

    Change Style

--- ui/template/container/archive.php
+++ ui/template/container/archive.php
@@ -13,19 +13,24 @@ krsort($temp);
 	<?php
 	
 	foreach((array)$temp as $month => $article_list) {
-		echo '<div>';
+		echo '<div class="month_archive">';
 		echo '<div class="month">' . $month . '</div>';
 		echo '<div class="list">';
 		foreach((array)$article_list as $info) {
 			echo '<article>';
-			echo '<span>' . $info['title'] . '</span>';
+			echo '<span class="title"><a href="' . BLOG_PATH . 'article/' . $info['url'] . '">' . $info['title'] . '</a></span>';
 			// echo '<footer>';
-			echo '<span>Category: ' . $info['category'] . '</span>';
+			echo '<span class="category">Category: <a href="'. BLOG_PATH . 'category/' . $info['category'] .'">' . $info['category'] . '</a></span>';
+			echo '<span class="tag">Tag: ';
+			foreach((array)$info['tag'] as $index => $tag)
+				echo '<a href="'. BLOG_PATH . 'tag/' . $tag .'">' . $tag . '</a>' . (count($info['tag'])-1 > $index ? ', ' : '');
+			echo '</span>';
 			// echo '</footer>';
 			echo '</article>';
 		}
 		echo '</div>';
 		echo '</div>';
+		echo '<hr>';
 	}
 	?>
 </div>
