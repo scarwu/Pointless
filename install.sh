1705b812d0647f01f121493f3a5391e4c6ce612b
ScarWu <xneriscool@gmail.com>
2013-01-08 23:36:07 +0800

    fix install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #/bin/sh
-wget https://raw.github.com/scarwu/Pointless/master/bin/poi /usr/bin/poi
+wget https://raw.github.com/scarwu/Pointless/master/bin/poi -O /usr/bin/poi
 chmod +x /usr/bin/poi
