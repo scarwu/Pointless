3f5e3a8437eaaa88fbd47ee6f0c9b85e4564ee07
ScarWu <xneriscool@gmail.com>
2013-03-16 00:25:46 +0800

    refactor

--- src/Sample/Template/Classic/Slider/30_Category.php
+++ src/Sample/Template/Classic/Slider/30_Category.php
@@ -1,9 +1,9 @@
 <div class="category">
-	<div class="title"><?php echo linkTo(BLOG_PATH . 'category', 'Category'); ?></div>
+	<div class="title"><?=linkTo(BLOG_PATH . 'category', 'Category')?></div>
 	<div class="content">
 		<?php
 		foreach((array)$data as $key => $value)
-			echo '<span>' . linkTo(BLOG_PATH.'category/'.$key, $key.'('.count($value).')') . '</span>';
+			echo '<span>' . linkTo(BLOG_PATH . "archive/$key", sprintf("$key(%s)", count($value))) . '</span>';
 		?>
 	</div>
 </div>
