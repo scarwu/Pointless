c28944065aa77068f784cb082552034de70ba956
ScarWu <xneriscool@gmail.com>
2013-03-16 03:00:09 +0800

    change path define, template to theme

--- src/Boot.php
+++ src/Boot.php
@@ -16,10 +16,11 @@ define('LIBRARY', ROOT . 'Library/');
 
 require LIBRARY . 'GeneralFunction.php';
 
-// Blog Path
+/**
+ * User Data
+ */
 define('USER_DATA', $_SERVER['HOME'] . '/.pointless/');
 
-// User Data
 if(!file_exists(USER_DATA))
 	mkdir(USER_DATA, 0755, TRUE);
 
@@ -29,7 +30,12 @@ if(!file_exists(USER_DATA . 'Config.php'))
 // Require Config
 require USER_DATA . 'Config.php';
 
-// Markdown
+/**
+ * Markdown
+ */
+if(!defined('MARKDOWN_FOLDER'))
+	define('MARKDOWN_FOLDER', USER_DATA . 'Markdown/');
+
 if(!file_exists(MARKDOWN_FOLDER)) {
 	mkdir(MARKDOWN_FOLDER, 0755, TRUE);
 	recursiveCopy(ROOT . 'Sample/Markdown', MARKDOWN_FOLDER);
@@ -38,43 +44,68 @@ if(!file_exists(MARKDOWN_FOLDER)) {
 define('MARKDOWN_ARTICLE', MARKDOWN_FOLDER . 'Article/');
 define('MARKDOWN_BLOGPAGE', MARKDOWN_FOLDER . 'BlogPage/');
 
-// Template and Theme
-if(!file_exists(TEMPLATE_FOLDER)) {
-	mkdir(TEMPLATE_FOLDER, 0755, TRUE);
-	recursiveCopy(ROOT . 'Sample/Template', TEMPLATE_FOLDER);
+/**
+ * Theme
+ */
+if(!defined('THEME_FOLDER'))
+	define('THEME_FOLDER', USER_DATA . 'Theme/');
+
+if(!file_exists(THEME_FOLDER)) {
+	mkdir(THEME_FOLDER, 0755, TRUE);
+	recursiveCopy(ROOT . 'Sample/Theme', THEME_FOLDER);
 }
 
 // Test Theme Path
-if(file_exists(TEMPLATE_FOLDER . BLOG_THEME) && '' != BLOG_THEME)
-	define('THEME_FOLDER', TEMPLATE_FOLDER . BLOG_THEME. '/');
-elseif(file_exists(ROOT . 'Sample/Template/' . BLOG_THEME) && '' != BLOG_THEME)
-	define('THEME_FOLDER', ROOT . 'Sample/Template/' . BLOG_THEME. '/');
+if(file_exists(THEME_FOLDER . BLOG_THEME) && '' != BLOG_THEME)
+	define('THEME_PATH', THEME_FOLDER . BLOG_THEME. '/');
+elseif(file_exists(ROOT . 'Sample/Theme/' . BLOG_THEME) && '' != BLOG_THEME)
+	define('THEME_PATH', ROOT . 'Sample/Theme/' . BLOG_THEME. '/');
 else
-	define('THEME_FOLDER', ROOT . 'Sample/Template/Classic/');
-	
-define('THEME_JS', THEME_FOLDER . 'Js/');
-define('THEME_CSS', THEME_FOLDER . 'Css/');
-define('THEME_RESOURCE', THEME_FOLDER . 'Resource/');
-define('THEME_CONTAINER', THEME_FOLDER . 'Container/');
-define('THEME_SLIDER', THEME_FOLDER . 'Slider/');
+	define('THEME_PATH', ROOT . 'Sample/Theme/Classic/');
 
-// Theme Script 
+define('THEME_JS', THEME_PATH . 'Js/');
+define('THEME_CSS', THEME_PATH . 'Css/');
+define('THEME_RESOURCE', THEME_PATH . 'Resource/');
+define('THEME_CONTAINER', THEME_PATH . 'Container/');
+define('THEME_SLIDER', THEME_PATH . 'Slider/');
+
+/**
+ * Script
+ */
+if(!defined('SCRIPT_FOLDER'))
+	define('SCRIPT_FOLDER', USER_DATA . 'Script/');
+
 if(!file_exists(SCRIPT_FOLDER)) {
 	mkdir(SCRIPT_FOLDER, 0755, TRUE);
 	recursiveCopy(ROOT . 'Sample/Script', SCRIPT_FOLDER);
 }
 
-// Extension
+/**
+ * Extension
+ */
+if(!defined('EXTENSION_FOLDER'))
+	define('EXTENSION_FOLDER', USER_DATA . 'Extension/');
+
 if(!file_exists(EXTENSION_FOLDER)) {
 	mkdir(EXTENSION_FOLDER, 0755, TRUE);
 	recursiveCopy(ROOT . 'Sample/Extension', EXTENSION_FOLDER);
 }
 
-// Public
+/**
+ * Public
+ */
+if(!defined('PUBLIC_FOLDER'))
+	define('PUBLIC_FOLDER', USER_DATA . 'Public/');
+
 if(!file_exists(PUBLIC_FOLDER))
 	mkdir(PUBLIC_FOLDER, 0755, TRUE);
 
-// Resource
+/**
+ * Resource
+ */
+if(!defined('RESOURCE_FOLDER'))
+	define('RESOURCE_FOLDER', USER_DATA . 'Resource/');
+
 if(!file_exists(RESOURCE_FOLDER))
 	mkdir(RESOURCE_FOLDER, 0755, TRUE);
 
