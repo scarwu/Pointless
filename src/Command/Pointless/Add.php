1a9f0ad6bdde9842e767103cb9be49f28762f326
ScarWu <xneriscool@gmail.com>
2013-04-01 23:27:16 +0800

    refactor command add

--- src/Command/Pointless/Add.php
+++ src/Command/Pointless/Add.php
@@ -19,76 +19,62 @@ class Add extends Command {
 	}
 	
 	public function run() {
-		if($this->hasOptions('s')) {
-			$this->staticPage();
-			return;
-		}
-
-		$this->article();
-	}
-
-	public function article() {
-		$info = array(
-			'title' => IO::question("Enter Title:\n-> "),
-			'url' => IO::question("Enter Custom Url:\n-> "),
-			'tag' => IO::question("Enter Tag:\n-> "),
-			'category' => IO::question("Enter Category:\n-> ")
-		);
-		
-		if(NULL != LOCAL_ENCODING)
-			foreach($info as $key => $value)
-				$info[$key] = iconv(LOCAL_ENCODING, 'utf-8', $value);
-
-		$time = time();
-		$filename = sprintf("%s%s.md", date("Ymd_", $time), $info['url']);
-
-		if(!file_exists(MARKDOWN_FOLDER . $filename)) {
-			$handle = fopen(MARKDOWN_FOLDER . $filename, 'w+');
-			fwrite($handle, "{\n");
-			fwrite($handle, '	"type": "article",' . "\n");
-			fwrite($handle, '	"title": "' . $info['title'] . '",' . "\n");
-			fwrite($handle, '	"url": "' . $info['url'] . '",' . "\n");
-			fwrite($handle, '	"tag": "' . $info['tag'] . '",' . "\n");
-			fwrite($handle, '	"category": "' . $info['category'] . '",' . "\n");
-			fwrite($handle, '	"date": "' . date("Y-m-d", $time) . '",' . "\n");
-			fwrite($handle, '	"time": "' . date("H:i:s", $time) . '",' . "\n");
-			fwrite($handle, '	"message": true,' . "\n");
-			fwrite($handle, '	"publish": false' . "\n");
-			fwrite($handle, "}\n\n\n");
-			
-			IO::writeln("\nArticle $filename was created.");
-			system(FILE_EDITOR . ' ' . MARKDOWN_FOLDER . "$filename < `tty` > `tty`");
-		}
-		else
-			IO::writeln("\nArticle $filename is exsist.");
-	}
-
-	public function staticPage() {
 		$info = array(
 			'title' => IO::question("Enter Title:\n-> "),
 			'url' => IO::question("Enter Custom Url:\n-> ")
 		);
-		
+
+		if(!$this->hasOptions('s')) {
+			$info['tag'] = IO::question("Enter Tag:\n-> ");
+			$info['category'] = IO::question("Enter Category:\n-> ");
+		}
+
 		if(NULL != LOCAL_ENCODING)
 			foreach($info as $key => $value)
 				$info[$key] = iconv(LOCAL_ENCODING, 'utf-8', $value);
 
-		$filename = str_replace(array('\\', '/', ' '), '-', $info['title']);
-		$filename = 'static_' . strtolower($filename) . '.md';
+		if($this->hasOptions('s')) {
+			$filename = str_replace(array('\\', '/', ' '), '-', $info['title']);
+			$filename = 'static_' . strtolower($filename) . '.md';
 
-		if(!file_exists(MARKDOWN_FOLDER . $filename)) {
-			$handle = fopen(MARKDOWN_FOLDER . $filename, 'w+');
-			fwrite($handle, "{\n");
-			fwrite($handle, '	"type": "static",' . "\n");
-			fwrite($handle, '	"title": "' . $info['title'] . '",' . "\n");
-			fwrite($handle, '	"url": "' . $info['url'] . '",' . "\n");
-			fwrite($handle, '	"message": false' . "\n");
-			fwrite($handle, "}\n\n\n");
-			
-			IO::writeln("\nStatic Page $filename was created.");
-			system(FILE_EDITOR . ' ' . MARKDOWN_FOLDER . "$filename < `tty` > `tty`");
+			if(!file_exists(MARKDOWN_FOLDER . $filename)) {
+				$handle = fopen(MARKDOWN_FOLDER . $filename, 'w+');
+				fwrite($handle, "{\n");
+				fwrite($handle, '	"type": "static",' . "\n");
+				fwrite($handle, '	"title": "' . $info['title'] . '",' . "\n");
+				fwrite($handle, '	"url": "' . $info['url'] . '",' . "\n");
+				fwrite($handle, '	"message": false' . "\n");
+				fwrite($handle, "}\n\n\n");
+				
+				IO::writeln("\nStatic Page $filename was created.");
+				system(FILE_EDITOR . ' ' . MARKDOWN_FOLDER . "$filename < `tty` > `tty`");
+			}
+			else
+				IO::writeln("\nStatic Page $filename is exsist.");
 		}
-		else
-			IO::writeln("\nStatic Page $filename is exsist.");
+		else {
+			$time = time();
+			$filename = sprintf("%s%s.md", date("Ymd_", $time), $info['url']);
+
+			if(!file_exists(MARKDOWN_FOLDER . $filename)) {
+				$handle = fopen(MARKDOWN_FOLDER . $filename, 'w+');
+				fwrite($handle, "{\n");
+				fwrite($handle, '	"type": "article",' . "\n");
+				fwrite($handle, '	"title": "' . $info['title'] . '",' . "\n");
+				fwrite($handle, '	"url": "' . $info['url'] . '",' . "\n");
+				fwrite($handle, '	"tag": "' . $info['tag'] . '",' . "\n");
+				fwrite($handle, '	"category": "' . $info['category'] . '",' . "\n");
+				fwrite($handle, '	"date": "' . date("Y-m-d", $time) . '",' . "\n");
+				fwrite($handle, '	"time": "' . date("H:i:s", $time) . '",' . "\n");
+				fwrite($handle, '	"message": true,' . "\n");
+				fwrite($handle, '	"publish": false' . "\n");
+				fwrite($handle, "}\n\n\n");
+				
+				IO::writeln("\nArticle $filename was created.");
+				system(FILE_EDITOR . ' ' . MARKDOWN_FOLDER . "$filename < `tty` > `tty`");
+			}
+			else
+				IO::writeln("\nArticle $filename is exsist.");
+		}
 	}
 }
