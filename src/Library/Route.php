df567419c538bc7091a18339813cb4ae987c2f48
ScarWu <xneriscool@gmail.com>
2013-04-10 08:11:26 +0800

    finish route rule(prototype)

--- src/Library/Route.php
+++ src/Library/Route.php
@@ -8,14 +8,30 @@
  * @link		http://github.com/scarwu/Pointless
  */
 
-print_r($_SERVER);
-
 $status = json_decode(file_get_contents($_SERVER['DOCUMENT_ROOT'] . '/status.json'), TRUE);
 
 require $status['list'][$status['current']] . '/Config.php';
+$path = $status['list'][$status['current']] . '/Public/';
 
-$path = str_replace('/', '\/', BLOG_PATH);
-if(!preg_match("/^$path/", $_SERVER['REQUEST_URI']))
+$pattern = '/^' . str_replace('/', '\/', BLOG_PATH) . '/';
+if(!preg_match($pattern, $_SERVER['REQUEST_URI']))
 	header("Location:http://{$_SERVER['HTTP_HOST']}" . BLOG_PATH);
 
-echo $_SERVER['REQUEST_URI'];
+$pattern = '/^' . str_replace('/', '\/', BLOG_PATH) . '(.+)/';
+if(preg_match($pattern, $_SERVER['REQUEST_URI'], $match))
+	$path .= $match[1];
+
+$path .= is_dir($path) ? '/index.html' : '';
+
+if(preg_match('/\.css$/', $path))
+	header('Content-type: text/css');
+
+if(preg_match('/\.js$/', $path))
+	header('Content-type: text/javascript');
+
+if(file_exists($path)) {
+	echo file_get_contents($path);
+	return TRUE;
+}
+
+return FALSE;
