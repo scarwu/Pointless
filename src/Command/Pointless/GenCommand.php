3062315d778b9467695e4b6ed3066a63fa6f2747
ScarWu <xneriscool@gmail.com>
2014-01-11 12:33:36 +0800

    v1.2.4

--- src/Command/Pointless/GenCommand.php
+++ src/Command/Pointless/GenCommand.php
@@ -4,7 +4,7 @@
  * 
  * @package		Pointless
  * @author		ScarWu
- * @copyright	Copyright (c) 2012-2013, ScarWu (http://scar.simcz.tw/)
+ * @copyright	Copyright (c) 2012-2014, ScarWu (http://scar.simcz.tw/)
  * @link		http://github.com/scarwu/Pointless
  */
 
