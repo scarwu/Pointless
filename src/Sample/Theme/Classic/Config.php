90183928b4ccf041a447c7a4fdcce14d6acc047e
ScarWu <xneriscool@gmail.com>
2014-02-19 20:58:41 +0800

    now, theme can using config to toggle css/js/html, also can ordering item

---
+++ src/Sample/Theme/Classic/Config.php
@@ -0,0 +1,21 @@
+<?php
+$config = [
+    'js' => [
+        'modernizr-2.6.2.min.js',
+        '20_highlight.pack.js',
+        'custom.js'
+    ],
+    'css' => [
+        'normalize.css',
+        'main.css',
+        'index.css',
+        'solarized_dark.css'
+    ],
+    'template' => [
+        'side' => [
+            'Tag.php',
+            'Category.php',
+            'Archive.php'
+        ]
+    ]
+];
