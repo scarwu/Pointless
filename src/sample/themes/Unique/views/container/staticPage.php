13845cb1e9d6a9942c381d07474116671935d03c
Scar Wu <xneriscool@gmail.com>
2018-08-14 22:39:35 +0800

    1. remove old themes

--- src/sample/themes/Unique/views/container/staticPage.php
+++
@@ -1,9 +0,0 @@
-<div id="container_static">
-    <article class="post_block">
-        <h1 class="title"><?=$post['title']?></h1>
-        <div class="content"><?=$post['content']?></div>
-    </article>
-    <?php if(null != $blog['disqus_shortname'] && $post['message']): ?>
-    <div id="disqus_thread"></div>
-    <?php endif; ?>
-</div>
