4093abfad4915331b937de0b75c4fab2953ab904
ScarWu <xneriscool@gmail.com>
2018-10-02 14:35:36 +0800

    1. update setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -9,10 +9,3 @@ git submodule update
 # Install Composer Packages
 composer install
 
-# Install Node Packages
-yarn install
-
-# Build Modernizor
-cd ./node_modules/modernizr
-npm install
-./bin/modernizr -c lib/config-all.json
